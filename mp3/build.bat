a865709a9dda4d3dcc4f96184cd07552aa61296f
thomas.tom.mueller <thomas.tom.mueller@e3203527-bd36-0410-b05b-079c8a152bb5>
2008-04-14 19:53:06 +0000

    change the build tool

---
+++ mp3/build.bat
@@ -0,0 +1,7 @@
+@echo off
+if "%JAVA_HOME%"=="" echo Error: JAVA_HOME is not defined.
+if exist bin/org/mp3transform/build/Build.class goto buildOK
+if not exist bin mkdir bin
+javac -sourcepath src/tools -d bin src/tools/org/mp3transform/build/*.java
+:buildOK
+java -cp "bin;%JAVA_HOME%/lib/tools.jar;target" org.mp3transform.build.Build %1 %2 %3 %4 %5
---
+++ mp3/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ -z "$JAVA_HOME" ] ; then
+  echo "Error: JAVA_HOME is not defined."
+fi
+if [ ! -f "bin/org/mp3transform/build/Build.class" ] ; then
+  if [ ! -d "bin" ] ; then
+    mkdir bin
+  fi
+  javac -sourcepath src/tools -d bin src/tools/org/mp3transform/build/*.java
+fi
+java -cp "bin:$JAVA_HOME/lib/tools.jar:target" org.mp3transform.build.Build $@
